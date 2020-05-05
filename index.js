c8dda07d8c83b79ca79a7960506b135a230314f6
Zarina Gumbatova <26606767+Zarina2311@users.noreply.github.com>
2020-05-04 20:20:34 -0700

    move to new repo

---
+++ index.js
@@ -0,0 +1,66 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const TrakkerDatabase = require("./trakker-database");
+
+const DEFAULT_PORT = 3001;
+const PORT = process.env.PORT || DEFAULT_PORT;
+const db = TrakkerDatabase();
+const api = express();
+
+api.use(bodyParser.urlencoded({ extended: false }));
+
+// to get all columns
+api.get("/user/:user_id", (req, res) =>
+  db
+    .getColumns({
+      user_id: req.params.user_id,
+    })
+    .then((columns) => res.send(columns))
+    .catch((err) => {
+      // TODO figure out which status to send based on the err object
+      res.send([]);
+    })
+);
+
+// to create a new column
+api.post("/user/:user_id", (req, res) => {
+  db.addColumn({
+    user_id: req.params.user_id,
+    name: req.body.name,
+  })
+    .then((column) => res.send(column))
+    .catch((err) => {
+      console.error(err);
+      res.send([]);
+    });
+});
+
+// to get all cards for a column
+api.get("/user/:user_id/:col_id", (req, res) =>
+  db
+    .getCardsForColumn({
+      col_id: req.params.col_id,
+    })
+    .then((cardList) => res.send(cardList))
+    .catch((err) => {
+      console.error(err);
+      res.send([]);
+    })
+);
+
+// to create a card for a column
+api.post("/user/:user_id/:col_id", (req, res) => {
+  db.addCardToColumn({
+    col_id: req.params.col_id,
+    name: req.body.name,
+  })
+    .then((card) => res.send(card))
+    .catch((err) => {
+      console.error(err);
+      res.send([]);
+    });
+});
+
+api.listen(PORT, () => {
+  console.log(`trakker express api listening on port ${PORT}`);
+});
---
+++ trakker-database.js
@@ -0,0 +1,80 @@
+const pgp = require("pg-promise")();
+const dbName = process.env.DB_NAME || "trakker";
+
+const ignoreEmptyResultsError = (err) => {
+  if (err.code !== 0) {
+    throw new Error(err);
+  } else {
+    return [];
+  }
+};
+
+module.exports = () => {
+  const connectionString = `postgres://localhost:5432/${dbName}`;
+  const db = pgp(connectionString);
+
+  console.log("Postgres DB => ", connectionString);
+  db.one("SELECT true AS success")
+    .then((res) => `Test query succeeded: ${res.success}`)
+    .then(console.log);
+
+  const getColumns = ({ user_id }) =>
+    db
+      .many(
+        `
+      SELECT
+        id,
+        name
+      FROM col
+      WHERE col.user_id = $1
+    `,
+        [user_id]
+      )
+      .catch(ignoreEmptyResultsError);
+
+  const addColumn = ({ user_id, name }) =>
+    db
+      .many(
+        `
+      INSERT INTO col(user_id, name) 
+      VALUES ($1, $2)
+      RETURNING *
+    `,
+        [user_id, name]
+      )
+      .catch(ignoreEmptyResultsError);
+
+  const getCardsForColumn = ({ col_id }) =>
+    db
+      .many(
+        `
+      SELECT
+        id,
+        col_id,
+        name
+      FROM card
+      WHERE card.col_id = $1
+    `,
+        [col_id]
+      )
+      .catch(ignoreEmptyResultsError);
+
+  const addCardToColumn = ({ col_id, name }) =>
+    db
+      .many(
+        `
+      INSERT INTO card(col_id, name) 
+      VALUES ($1, $2)
+      RETURNING *
+    `,
+        [col_id, name]
+      )
+      .catch(ignoreEmptyResultsError);
+
+  return {
+    getColumns,
+    addColumn,
+    addCardToColumn,
+    getCardsForColumn,
+  };
+};
